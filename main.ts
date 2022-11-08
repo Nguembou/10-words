9a6822004b06e52fd49caf4c06d1995977e90c61
Nguembou <113910948+Nguembou@users.noreply.github.com>
2022-11-08 09:35:13 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("" + (text_list[0]))
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("" + (text_list[9]))
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("" + (text_list[2]))
+})
+let text_list: string[] = []
+text_list = [
+"Elwood",
+"Walton",
+"Wynn",
+"Dale",
+"Doyle",
+"Daryl",
+"Dan",
+"Don",
+"Earl",
+"Ernie"
+]
+basic.forever(function () {
+	
+})
